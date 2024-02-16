74418e61bac5f36b8d733f9d9b5ec4715202ac8d
Vidhi Shah <vidhishah@Vidhis-MacBook-Air.local>
2024-02-16 13:01:32 -0500

    created branch and added empty checks

--- quirknotes/backend/tests/status200.test.js
+++ quirknotes/backend/tests/status200.test.js
@@ -25,3 +25,48 @@ test("/postNote - Post a note", async () => {
   expect(postNoteRes.status).toBe(200);
   expect(postNoteBody.response).toBe("Note added succesfully.");
 });
+
+test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
+  
+  test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
+  
+  test("/deleteNote - Delete a note", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
+  
+  test("/patchNote - Patch with content and title", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
+  
+  test("/patchNote - Patch with just title", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
+  
+  test("/patchNote - Patch with just content", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
+  
+  test("/deleteAllNotes - Delete one note", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
+  
+  test("/deleteAllNotes - Delete three notes", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
+  
+  test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
+    // Code here
+    expect(false).toBe(true);
+  });
